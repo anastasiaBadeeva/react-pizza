684dcfbc63aae0eb1b88b41b11afbf1aea4994ec
anastasiaBadeeva <abadeeva092@gmail.com>
2021-01-24 16:58:13 +0300

    created SortPopUp component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react'
 
 const Categories = ({items,onClick}) => {
     const [activItem, setActivItem] = useState(null)
+    const onSelectItem = (index) =>{
+        setActivItem(index)
+    }
     return (
         <div className="categories">
                 <ul>
-                  <li>Все</li>
-                 {
-                     items.map((name,index)=>(<li key= {`${name}_${index}`} onClick= {()=>{setActivItem(index)}}
-                     className = {activItem === index ? "active" :null} >
+                  <li  className = {activItem === null ? "active" : ""} onClick= {()=>{onSelectItem(null)}}>Все</li>
+                 {items &&
+                     items.map((name,index)=>(<li key= {`${name}_${index}`} onClick= {()=>{onSelectItem(index)}}
+                     className = {activItem === index ? "active" : ""} >
                          {name}
                          </li>))
                  }
